25505c5d3e7e92da5b352477e3430da8bfbee2c9
Charles Cosse <ccosse@gmail.com>
2017-04-28 08:20:43 -0600

    code clean-up; jquery ajax improvs; eliminate forms; sending keepalives to rpi3

--- daemons/utils.py
+++ daemons/utils.py
@@ -1,4 +1,4 @@
-import time
+import time,os
 def mkDeviceOptions():
 	opts={'keys':[],}
 	inf=open('/var/lib/misc/dnsmasq.leases')
@@ -19,3 +19,83 @@ def mkDeviceOptions():
 		opts['keys'].append(key)
 
 	return opts
+
+def getAirOnly():
+	air_only=[
+		"iptables -A FORWARD  -m mac --mac-source B8:E8:56:26:E4:B0 -j ACCEPT"
+	]
+	return air_only
+
+def getMACOnly(MAC):
+	mac_only=[
+		"iptables -A FORWARD  -m mac --mac-source %s -j ACCEPT"%(MAC)
+	]
+	return mac_only
+
+def getWideOpen():
+	LAN0="eth0"
+	LAN1="wlan1"
+	WAN="wlan0"
+	wide_open=[
+		"iptables -F",
+		"iptables -t nat -F",
+		"iptables -P INPUT ACCEPT",
+		"iptables -P OUTPUT ACCEPT",
+		"iptables -P FORWARD ACCEPT",
+		"iptables -I INPUT 1 -i %s -j ACCEPT"%(LAN0),
+		"iptables -I INPUT 1 -i %s -j ACCEPT"%(LAN1),
+		"iptables -I INPUT 1 -i lo -j ACCEPT",
+		"iptables -A INPUT -p UDP --dport bootps ! -i %s -j REJECT"%(LAN0),
+		"iptables -A INPUT -p UDP --dport bootps ! -i %s -j REJECT"%(LAN1),
+		"iptables -A INPUT -p UDP --dport domain ! -i %s -j REJECT"%(LAN0),
+		"iptables -A INPUT -p UDP --dport domain ! -i %s -j REJECT"%(LAN1),
+		"iptables -A INPUT -p TCP --dport ssh -i %s -j ACCEPT"%(WAN),
+		#"iptables -A INPUT -p TCP ! -i ${LAN} -d 0/0 --dport 0:1023 -j DROP",
+		#"iptables -A INPUT -p UDP ! -i ${LAN} -d 0/0 --dport 0:1023 -j DROP",
+		"iptables -I FORWARD -i %s -d 192.168.0.0/255.255.0.0 -j ACCEPT"%(LAN0),
+		"iptables -I FORWARD -i %s -d 192.168.0.0/255.255.0.0 -j ACCEPT"%(LAN1),
+		"iptables -A FORWARD -i %s -s 192.168.0.0/255.255.0.0 -j ACCEPT"%(LAN0),
+		"iptables -A FORWARD -i %s -s 192.168.0.0/255.255.0.0 -j ACCEPT"%(LAN1),
+		"iptables -A FORWARD -i %s -d 192.168.0.0/255.255.0.0 -j ACCEPT"%(WAN),
+		"iptables -t nat -A POSTROUTING -o %s -j MASQUERADE"%(WAN),
+		"echo 1 > /proc/sys/net/ipv4/ip_forward",
+	]
+	return wide_open
+
+def getWideClosed():
+	LAN0="eth0"
+	LAN1="wlan1"
+	WAN="wlan0"
+	wide_closed=[
+		"iptables -F",
+		"iptables -t nat -F",
+		"iptables -P INPUT ACCEPT",
+		"iptables -P OUTPUT ACCEPT",
+		"iptables -P FORWARD DROP",
+		"iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT",
+		"iptables -I INPUT 1 -i %s -j ACCEPT"%(LAN1),
+		"iptables -I INPUT 1 -i lo -j ACCEPT",
+		"iptables -A INPUT -p UDP --dport bootps ! -i %s -j REJECT"%(LAN1),
+		"iptables -A INPUT -p UDP --dport domain ! -i %s -j REJECT"%(LAN1),
+		"iptables -A INPUT -p TCP ! -i %s -d 0/0 --dport 0:1023 -j DROP"%(LAN1),
+		"iptables -A INPUT -p UDP ! -i %s -d 0/0 --dport 0:1023 -j DROP"%(LAN1),
+	]
+	return wide_closed
+
+def setWideOpen():
+	wide_open=getWideOpen()
+	for gidx in range(len(wide_open)):
+		ip_cmd=wide_open[gidx]
+		print(ip_cmd)
+		os.system(ip_cmd)
+	os.system('iptables -S')
+	return 'success'
+
+def setWideClosed():
+	wide_closed=getWideClosed()
+	for gidx in range(len(wide_closed)):
+		ip_cmd=wide_closed[gidx]
+		print(ip_cmd)
+		os.system(ip_cmd)
+	os.system('iptables -S')
+	return 'success'
--- views.py
+++ views.py
@@ -8,13 +8,13 @@ from django.contrib.auth.decorators import login_required,user_passes_test
 
 from creditmeter.daemons.utils import *
 
-import os,logging,time,json,sys,urllib.request
+import os,logging,time,json,sys,urllib.request,random
 import xmlrpc.client
 
 FORMAT = 'VIEW: %(message)s'
 
-RLOG_FULL_PATH='./meter.log'
-logging.basicConfig(filename=RLOG_FULL_PATH,level=logging.DEBUG, format=FORMAT)
+CREDITMETER_LOG_FULL_PATH='./meter.log'
+logging.basicConfig(filename=CREDITMETER_LOG_FULL_PATH,level=logging.DEBUG, format=FORMAT)
 mylogger = logging.getLogger('django')
 
 def logout_view(request):
@@ -22,107 +22,95 @@ def logout_view(request):
     empty_session_id=logout(request)
     logging.debug("empty_session_id: %s"%empty_session_id)
     request.session.delete()
-    return HttpResponseRedirect("/meter")
+    return HttpResponseRedirect("/")
+
+def keepalive(request):
+	ip=request.META.get('HTTP_X_FORWARDED_FOR') or request.META.get('REMOTE_ADDR')
+	mylogger.debug("keepalive "+ip)
+	#mylogger.debug('old '+request.POST['old'])
+	return HttpResponse(int(random.random()*5000.))
 
 def home(request):
 	mylogger.debug("home");
 	if request.user.is_authenticated():
 		mylogger.debug("already authenticated:"+request.user.username)
 		return app(request)
-	else:
-		ip=request.META.get('HTTP_X_FORWARDED_FOR') or request.META.get('REMOTE_ADDR')
-		mylogger.debug("not authenticated: %s"%(ip))
-		if request.method == 'POST':
-			mylogger.debug(request.POST["pyld"])
-			pyld=json.loads(request.POST["pyld"])
-			try:
-				x=User.objects.get(username=pyld['device_name'])
-				mylogger.debug("got user")
-				login(request,x)
-				mylogger.debug("logged-in user")
-				return app(request)
-			except:
-				mylogger.debug("creating new account ...")
-				acct=User.objects.create_user(username=pyld['device_name'],password='pycon2017')
-				acct.userprofile.is_parent=False
-				acct.userprofile.remote_username="guest"
-				acct.userprofile.remote_password="pycon2017"
-				acct.userprofile.save()
-				acct.save()
-				mylogger.debug("logging-in ...")
-				login(request,acct)
-				return app(request)
 
-		device_options=mkDeviceOptions()
-		IP=request.META.get('HTTP_X_FORWARDED_FOR') or request.META.get('REMOTE_ADDR')
-		opt=device_options[IP]
+	ip=request.META.get('HTTP_X_FORWARDED_FOR') or request.META.get('REMOTE_ADDR')
+	mylogger.debug("not authenticated: %s"%(ip))
+	if request.method == 'POST':
+		mylogger.debug("getting login_pyld from post ...")
+		mylogger.debug(request.POST["login_pyld"])
+		pyld=json.loads(request.POST["login_pyld"])
+		try:
+			x=User.objects.get(username=pyld['device_name'])
+			mylogger.debug("got user")
+			login(request,x)
+			mylogger.debug("logged-in user")
+			return app(request)
+		except:
+			mylogger.debug("creating new account ...")
+			acct=User.objects.create_user(username=pyld['device_name'],password='pycon2017')
+			acct.userprofile.is_parent=False
+			acct.userprofile.remote_username="guest"
+			acct.userprofile.remote_password="pycon2017"
+			acct.userprofile.save()
+			acct.save()
+			mylogger.debug("logging-in ...")
+			login(request,acct)
+			return app(request)
 
-		context={
-			'title':'Credit Meter Login',
-			'button_text':'Login',
-			'lease_time':opt['lease_time'],
-			'human_lease_time':opt['human_lease_time'],
-			'device_mac':opt['device_mac'],
-			'device_ip':opt['device_ip'],
-			'device_name':opt['device_name'],
-			'xtra_mac':opt['xtra_mac'],
-		}
-
+	mylogger.debug("home, not a post")
+	device_options=mkDeviceOptions()
+	IP=request.META.get('HTTP_X_FORWARDED_FOR') or request.META.get('REMOTE_ADDR')
+	opt=device_options[IP]
+	context={
+		'title':'Credit Meter Login',
+		'button_text':'Login',
+		'lease_time':opt['lease_time'],
+		'human_lease_time':opt['human_lease_time'],
+		'device_mac':opt['device_mac'],
+		'device_ip':opt['device_ip'],
+		'device_name':opt['device_name'],
+		'xtra_mac':opt['xtra_mac'],
+	}
+	mylogger.debug(json.dumps(context))
 	return render(
 		request,'device-login.html',#login.html
 		context
 	)
 
 @login_required
+def get(request):#remote balance query and xfer @here, plus others.
+	rval=-999
+	credit_feeder_url="http://feeder.asymptopia.org/"
+	try:
+		qs=request.META['QUERY_STRING']
+
+		if qs=='local_balance':
+			rval=request.user.userprofile.credit_balance
+
+		elif qs=='remote_balance':
+			url='%sget?request=update&username=%s&password=%s'%(credit_feeder_url,request.user.userprofile.remote_username,request.user.userprofile.remote_password)
+			with urllib.request.urlopen(url) as response:
+				remote_balance = int(response.read())
+			rval=remote_balance
+
+		elif qs=='transfer_balance':
+			url='%sget?request=transfer&username=%s&password=%s'%(credit_feeder_url,request.user.userprofile.remote_username,request.user.userprofile.remote_password)
+			mylogger.debug(url)
+			with urllib.request.urlopen(url) as response:
+				transfer_amount=int(response.read())
+			request.user.userprofile.credit_balance+=transfer_amount
+			request.user.userprofile.save()
+			rval=transfer_amount
+
+	except:mylogger.exception("UhOh")
+	return HttpResponse(rval);
+
+@login_required
 def app(request):
-	mylogger.debug("HOME")
-	if request.method=='POST':
-		try:
-			mylogger.debug("POST")
-			str_pyld=request.POST["pyld"]
-			json_pyld=json.loads(str_pyld)
-			mylogger.debug(str_pyld)
-			s=xmlrpc.client.Server("http://192.168.22.1:8210")
-
-			if json_pyld['action']=='update_remote_balance':
-				mylogger.debug("update_remote_balance")
-				#rval=s.update_remote_balance(request.user.username,request.user.userprofile.remote_username,request.user.userprofile.remote_password);
-				url='http://feeder.asymptopia.org/get?request=update&username=%s&password=%s'%(request.user.userprofile.remote_username,request.user.userprofile.remote_password)
-				mylogger.debug(url)
-				with urllib.request.urlopen(url) as response:
-					remote_balance = int(response.read())
-				#response.close()
-				rval={'remote_balance':remote_balance}
-
-			elif json_pyld['action']=='transfer_remote_balance':
-				mylogger.debug("transfer_remote_balance")
-				url='http://feeder.asymptopia.org/get?request=transfer&username=%s&password=%s'%(request.user.userprofile.remote_username,request.user.userprofile.remote_password)
-				mylogger.debug(url)
-				with urllib.request.urlopen(url) as response:
-					transfer_amount=int(response.read())
-				remote_balance=0#it must if no errors
-				#response.close()
-				request.user.userprofile.credit_balance+=transfer_amount
-				request.user.userprofile.save()
-				mylogger.debug("userprofile updated by: "+str(transfer_amount))
-				rval={
-					'local_balance':request.user.userprofile.credit_balance,
-					'transfer_amount':transfer_amount,
-					'remote_balance':remote_balance,
-				}
-
-			else:
-				rval="unk"
-
-			mylogger.debug(rval)
-			return HttpResponse(json.dumps(rval))
-
-		except:
-			pass
-			#mylogger.exception("Something wrong ...")
-			#they're logged-in so give em the default page.  If they got here
-			#it's because POST=login in this case.
-
+	mylogger.debug("app")
 	context={
 		'title':'CreditMeter',
 		'username':request.user.username,
