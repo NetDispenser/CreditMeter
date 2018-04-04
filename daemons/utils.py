e405259e4dd734180e3938283df9d808943209fc
Charles Cosse <ccosse@gmail.com>
2018-04-03 23:51:31 -0600

    asdf

--- daemons/utils.py
+++ daemons/utils.py
@@ -4,7 +4,7 @@ CREDITMETER_LOG_FULL_PATH='/var/www/meter/creditmeter/meter.log'
 CREDITMETERD_PORT=8210
 CREDIT_FEEDER_URL="http://www.creditfeed.me/"
 CREDITMETER_PID='/var/run/creditmeter.pid'
-CREDITMETER_HOSTNAME="192.168.22.1"
+CREDITMETER_HOSTNAME="192.168.1.1"
 #CREDITMETER_HOSTNAME="meter.creditfeed.me"
 
 LAN0="eth0"
